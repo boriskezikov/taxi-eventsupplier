ba6e8bcb3a072f73abb38a7882a29beee14aed41
kezik <kezikov@vtb.ru>
2021-05-19 16:56:39 +0300

    separate logic

---
+++ src/test/java/ru/taxi/eventsupplier/EventsupplierApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.taxi.eventsupplier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventsupplierApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
