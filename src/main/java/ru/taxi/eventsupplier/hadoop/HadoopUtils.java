33e64c9f403cb2e095c0207775d1a2b8f43d18bb
kezik <kezikov@vtb.ru>
2021-06-03 01:02:39 +0300

    polishing

--- src/main/java/ru/taxi/eventsupplier/hadoop/HadoopUtils.java
+++ src/main/java/ru/taxi/eventsupplier/hadoop/HadoopUtils.java
@@ -6,6 +6,5 @@ import lombok.experimental.UtilityClass;
 public class HadoopUtils {
 
     public static final String RAW_DATA_DRIVE = "/data/csv/raw/";
-    public static final String TRAIN_REPORT_DRIVE = "/data/csv/report/";
     public static final String RAW_PREVIEW_FILENAME = "yellow_tripdata_2015-01.csv";
 }
